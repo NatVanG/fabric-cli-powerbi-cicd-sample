92002159126885c57a92beb063a6f7811f2b7f8c
Nat Van Gulck <107113953+NatVanG@users.noreply.github.com>
2025-06-19 14:45:52 +0100

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -67,7 +67,7 @@ def run_fab_command(
         )
 
     if capture_output:
-
+        print("fabcommand output: " + result[:250])
         output = result.stdout.strip().split("\n")[-1]
 
         return output
