33bd3d6c85b33c36da62ca0cbce5c1a4c4b0ea6c
Nat Van Gulck <107113953+NatVanG@users.noreply.github.com>
2025-06-24 09:57:04 +0100

    Update new-bpa.py

--- scripts/new-bpa.py
+++ scripts/new-bpa.py
@@ -52,6 +52,16 @@ def main():
         exe_path = find_executable(extract_path)
         print("Local executable path:")
         print(exe_path)
-        #run_executable(exe_path, ["--help"])  # Replace with your actual arguments
+        
+        current_path = os.getcwd()
+        print("Current working directory:", current_path)
+
+        args = [
+            "-fabricitem", "src",
+            "-rules", "rules\\Example-tenantSettings-rules.json",
+            "-formats", "GitHub"
+        ]
+
+        run_executable(exe_path, args)
 
 main()
