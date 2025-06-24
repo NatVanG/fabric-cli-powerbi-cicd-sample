e7322a141c89a371b66ef86457d7659777e0ea7f
Nat Van Gulck <nathaniel.vangulck@microsoft.com>
2025-06-24 10:12:58 +0100

    fixed new-bpa.py

--- scripts/new-bpa.py
+++ scripts/new-bpa.py
@@ -55,9 +55,9 @@ def main():
         
         current_path = os.getcwd()
         print("Current working directory:", current_path)
-
-        args = ["-fabricitem","src","-rules", "rules\\Example-tenantSettings-rules.json","-formats", "GitHub"]
-
+        
+        args = ["-fabricitem","src","-rules", "rules\\Example-tenantSettings-rules.json","-formats", "GitHub"]
+        
         run_executable(exe_path, args)
 
 main()
